6a5a837314ef17030d0e4b8a6577c65914a205bd
isaac alvarez silva <123046942@upq.edu.mx>
2025-09-25 16:44:14 -0600

    Commit1
    ejericio1

---
+++ Practica3/main.js
@@ -0,0 +1,6 @@
+const restar = require('./utils.js');
+
+console.log("5 - 3 =", restar(5, 3));
+console.log("11 - 4 =", restar(11, 4));
+console.log("45  30 =", restar(45, 30));
+console.log("50 - 23 =", restar(50, 23));

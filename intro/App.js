ca8b2ba6737dc161a770c7c96fd0ffed7be6dbc9
isaac alvarez silva <123046942@upq.edu.mx>
2025-10-09 17:17:52 -0600

    Practica5
    Commit de la practica 5 (contador)

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,27 @@
+//1. import: zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react';
 
+//2. main: zona de componentes 
 export default function App() {
+
+  const [contador, setContador]=useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo web soy Isaac!!</Text>
+
+      <Text>Contador: {contador}</Text>
+      <Button title='Agregar' onPress={()=>setContador(contador+1)}/>
+      <Button title='Quitar' onPress={()=>setContador(contador-1)}/>
+      <Button title='Reiniciar' onPress={()=>setContador(0)}/>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//3. estilos: zona estetica y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,

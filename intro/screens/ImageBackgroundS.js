4bb0dd5f96f4b41499afa405e116bcdf6e31b063
isaac alvarez silva <123046942@upq.edu.mx>
2025-10-24 15:27:07 -0600

    ImageBackground & SlapshScreen: completada

---
+++ intro/screens/ImageBackgroundS.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet, View, SafeAreaView, ImageBackground } from 'react-native';
+
+export default function ImageBackgroundS() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return (
+      <View style={styles.splashContainer}>
+        <Text style={styles.splashText}>Bienvenido a la app</Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        source={{ uri: "https://wallpapershome.com/images/pages/ico_h/5708.jpg" }}
+        style={styles.background}
+      >
+        <View style={styles.overlay}>
+          <Text style={styles.title}>Bienvenidos!</Text>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    backgroundColor: '#000000ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splashText: {
+    fontSize: 24,
+    color: '#fff',
+  },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    color: '#fff',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+});
